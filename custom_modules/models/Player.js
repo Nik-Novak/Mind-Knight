a02925fc5a7e02b0b38ce75ee13f03f2637d1d14
Nik-Novak <novakn@mcmaster.ca>
2024-03-17 15:56:07 -0400

    DEBUG

--- custom_modules/models/Player.js
+++ custom_modules/models/Player.js
@@ -9,7 +9,7 @@ const { Game, RawGame } = require('./Game');
 const PlayerSchema = new Schema({
   steamID:          { type:String, required:true },
   name:             { type:String, required:true, default:"" },
-  gameIDs:          { type:[ExistingID(Game)], default:[] },
+  gameIDs:          { type:[mongoose.Types.ObjectId], default:[] },
   raw_gameIDs:      { type:[ExistingID(RawGame)], default:[] },
   tournamentIDs:    { type:[ExistingID('Tournament')], default:[] },
   elo:              { type:Number, default:1500 },
