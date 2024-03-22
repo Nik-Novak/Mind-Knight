2849e3810439d38a32c251ef28db372e0312ad72
Nik <novakn@mcmaster.ca>
2024-03-22 19:01:27 -0400

    updated elo to show latest elo

--- app/src/components/Elo/Elo.tsx
+++ app/src/components/Elo/Elo.tsx
@@ -8,8 +8,12 @@ type Props = {
 export default function Elo({elo, eloIncrement}:Props){
   if(elo && eloIncrement){
     let eloIncrementColor = eloIncrement > 0 ? '#25A165' : eloIncrement < 0 ? '#952C30' : '#FFFFFF';
-    let eloText = coloredText(Math.round(elo).toFixed(0), '#AD8432');
+    let eloText = coloredText(Math.round(elo + eloIncrement).toFixed(0), '#AD8432');
     let eloIncrementText = coloredText((eloIncrement > 0 ? '+':'')+Math.round(eloIncrement).toFixed(0), eloIncrementColor);
     return <Typography className="player-elo">{eloText} {eloIncrementText}</Typography>
+  }
+  else if(elo){
+    let eloText = coloredText(Math.round(elo).toFixed(0), '#AD8432');
+    return <Typography className="player-elo">{eloText}</Typography>
   }
 }
