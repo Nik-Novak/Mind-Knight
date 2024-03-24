367d1ab4e75d827452c68be4a5779389465b80dc
Nik <novakn@mcmaster.ca>
2024-03-24 15:57:16 -0400

    added polishing prisma extension

--- app/src/app/game/page.tsx
+++ app/src/app/game/page.tsx
@@ -10,6 +10,8 @@ import Nodes from "@/components/Nodes";
 import NodeTeamRejects from "@/components/NodeTeamRejects";
 import Players from "@/components/Players";
 import Turns from "@/components/Turns";
+import { database } from "@/utils/database/database";
+import { Prisma } from "@prisma/client";
 
 //React server component that securely runs on the server by default
 export default async function GamePage() {
@@ -17,21 +19,39 @@ export default async function GamePage() {
   const selectedNode:NodeNumber = 1;
   const selectedTurn:number = 1;
   const selectedSlot:PlayerSlot = 0;
+
+  // console.log(game.game_found);
+  // game.
+  let player = await database.player.findOrCreate({Level:30, Nickname:'2JZ 4U', Slot:0, Steamid:'76561198073023481'});
+  game.player_ids=[]
+  game.player_ids.push(player.id); let game_found = game.game_found;
+  // let uploadedGame = await database.game.create({data:{game_found:{Options:game_found.Options,  FirstPlayer:game_found.FirstPlayer, GuyRole:game_found.GuyRole, Hacker: game_found.Hacker, HackersAmount:game_found.HackersAmount, Map:game_found.Map, MatchType:game_found.MatchType, PlayerNumber:game_found.PlayerNumber, Type:game_found.Type, VoiceChat:game_found.VoiceChat, VoiceChatChannel:game_found.VoiceChatChannel, VoiceChatName:game_found.VoiceChatName, Hackers:game_found.Hackers, MissionInfo:game_found.MissionInfo, MissionMinhacks:game_found.MissionMinhacks}, updated_at:new Date()}});
+
+  // console.log('BEFORE', game.game_start);
+  let polishedGame = database.game.polish(game);
+  // console.log('AFTER', polishedGame.game_start);
+
+  // console.log('OG', Object.keys(game));
+  // console.log('POLISHED', polishedGame);
+  // console.log('POLISHED', Object.keys(polishedGame))
   
+  let uploadedGame = await database.game.create({data:polishedGame});
+  // console.log('UPLOADED', uploadedGame.id);
+
   return (
     <>
       <main id='content' className={styles.main}>
         <Stack className={styles.left}>
-          <Panel title="Chat" > <Chatlog chat={game.chat} game_players={game.players} /> </Panel>
+          <Panel title="Chat" > <Chatlog chat={game.chat} game_players={game.game_players} /> </Panel>
         </Stack>
         <Stack className={styles.center}>
-          <ImportantInfo selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.players} numPlayers={game.game_found.PlayerNumber}/>
-          <Players selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.players} game_end={game.game_end} />
-          <Turns selectedNode={selectedNode} selectedTurn={selectedTurn} game_players={game.players} />
+          <ImportantInfo selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.game_players} numPlayers={game.game_found.PlayerNumber}/>
+          <Players selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.game_players} game_end={game.game_end} />
+          <Turns selectedNode={selectedNode} selectedTurn={selectedTurn} game_players={game.game_players} />
         </Stack>
         <Stack className={styles.right}>
           <Nodes selectedNode={selectedNode} missions={game.missions||undefined} game_found={game.game_found} />
-          <NodeTeamRejects selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.players}/>
+          <NodeTeamRejects selectedNode={selectedNode} selectedTurn={selectedTurn} selectedSlot={selectedSlot} game_players={game.game_players}/>
         </Stack>
       </main>
     </>
--- app/src/utils/database/database.ts
+++ app/src/utils/database/database.ts
@@ -1,12 +1,58 @@
-import { PrismaClient, PlayerIdentity } from '@prisma/client'
+import { PrismaClient, PlayerIdentity, Prisma, Game } from '@prisma/client'
+// let modelnames = Prisma.dmmf.datamodel.models.map(m=>m.name); Value `in` modelnames
 
-const prismaClientSingleton = ()=>{
-  return new  PrismaClient().$extends({
+type NonNull<T> = Exclude<T, null | undefined>;
+
+const prismaClientSingleton= ()=>{
+  let prisma = new PrismaClient();
+  return prisma.$extends({
+    query:{
+      $allOperations({ model, operation, args, query }) {
+        /* your custom logic for modifying all Prisma Client operations here */
+        return query(args)
+      }
+    },
     model: {
+      $allModels:{
+        polish<T>(
+          this:T, 
+          valueToPolish:Prisma.Args<T, 'create'>['data'], //corresponding model input type
+          schemaName = Prisma.getExtensionContext(this).$name
+        ) :NonNull< Prisma.Result<T, undefined, 'findFirst'> >/* corresponding model return type */ {
+          // let t:Prisma.Result<T, undefined, 'findFirst'>;
+          /* TODO: explore Prisma.TypeMap */
+          let ctx = Prisma.getExtensionContext(this);
+          if(Array.isArray(valueToPolish))//@ts-expect-error
+            return valueToPolish.map(v=>this.polish(v, schemaName)) // handle arrays of schema values
+          if(typeof valueToPolish !== 'object' || valueToPolish === null)
+            return valueToPolish;
+          let schemaReference = Prisma.dmmf.datamodel.models.find(m=>m.name==schemaName) || Prisma.dmmf.datamodel.types.find(t=>t.name==schemaName);
+          // console.log('SCHEMA_NAME', schemaName);
+          // console.log('SCHEMA', schemaReference); //DEBUG
+          let polishedData = {} as NonNull< Prisma.Result<T, undefined, 'findFirst'> >;
+          schemaReference?.fields.forEach(field=>{
+            // console.log('FIELD', field); //DEBUG
+            let value = valueToPolish[field.name];
+            if(value==undefined) // SOURCE FROM DBNAME such as __v in the case that v doesnt exist
+              value=(field.dbName && valueToPolish[field.dbName])
+            // console.log('value', value); //DEBUG
+            if(field.kind === 'object') {
+              //@ts-expect-error
+              value = this.polish(value, field.type); //Prisma.getExtensionContext(this) this is helpful too, in case of differing order of definition
+            }
+            //@ts-expect-error
+            polishedData[field.name] = value;
+          });
+          // console.log('OG',modelName,Object.keys(valueToPolish)); //DEBUG
+          // console.log('POLISHED', modelName, Object.keys(polishedData)); //DEBUG
+          return polishedData;
+        }
+      },
       player: {
         async findOrCreate(playerIdentity:PlayerIdentity){
           return database.player.create({data:{
-            steamID: playerIdentity.Steamid,
+            level: playerIdentity.Level,
+            steam_id: playerIdentity.Steamid,
             name: playerIdentity.Nickname,
           }})
         },
@@ -22,8 +68,6 @@ declare global {
 export const database = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV === 'development') globalThis.prismaGlobal = database;
-
-
 
 //awesome post for features: https://github.com/prisma/prisma/issues/7161#issuecomment-1026317110
 //custom computed fields: https://www.prisma.io/docs/orm/prisma-client/queries/computed-fields
