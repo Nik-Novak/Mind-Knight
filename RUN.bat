33eae4bdf415cfca5644de92dcb52d68ef770780
Nik <novakn@mcmaster.ca>
2024-04-12 10:18:46 -0400

    added browser launch

--- RUN.bat
+++ RUN.bat
@@ -3,5 +3,7 @@ if not exist ".\node_modules" (
     call ..\npx --yes yarn install
     call ..\npx --yes yarn prisma generate
 )
-call ..\npx --yes yarn windows
+start call ..\npx --yes yarn windows
+timeout /t 5
+start "" cmd /c "start http://localhost:3000 && taskkill /f /im cmd.exe"
 pause
