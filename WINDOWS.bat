a52d0fe1aeca54dd338128cfc18248bfd2292580
Nik-Novak <novakn@mcmaster.ca>
2024-04-12 12:59:29 -0400

    Update WINDOWS.bat

--- WINDOWS.bat
+++ WINDOWS.bat
@@ -1,8 +1,8 @@
 cd .\app
 if not exist ".\node_modules" (
-    call ..\npx --yes yarn install
+    call ..\npx.cmd --yes yarn install
 )
-start call ..\npx --yes yarn windows
+start call ..\npx.cmd --yes yarn windows
 timeout /t 5
 start "" cmd /c "start http://localhost:3000 && taskkill /f /im cmd.exe"
 pause
