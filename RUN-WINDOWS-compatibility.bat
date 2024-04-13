1581065394ee262afcae7529108ee8aa8421a8f8
Nik <novakn@mcmaster.ca>
2024-04-13 11:34:53 -0400

    foresee a bug with new dependnecies

--- RUN-WINDOWS-compatibility.bat
+++ RUN-WINDOWS-compatibility.bat
@@ -2,10 +2,10 @@ set "PATH=%CD%;%PATH%"
 cd .\app
 call npx -v
 call node -v
-if not exist ".\node_modules" (
+@REM if not exist ".\node_modules" (
     call npx yarn install --network-timeout 100000
     call npx yarn prisma generate
-)
+@REM )
 @REM if not exist ".\.next\BUILD_ID" (
 @REM     @REM rmdir /s /q .\.next
 @REM     @REM call npx yarn build:windows
--- RUN-WINDOWS.bat
+++ RUN-WINDOWS.bat
@@ -2,10 +2,10 @@ set "PATH=%CD%;%PATH%"
 cd .\app
 call npx -v
 call node -v
-if not exist ".\node_modules" (
+@REM if not exist ".\node_modules" (
     call npx yarn install --network-timeout 100000
     call npx yarn prisma generate
-)
+@REM )
 if not exist ".\.next\BUILD_ID" (
     @REM rmdir /s /q .\.next
     @REM call npx yarn build:windows
