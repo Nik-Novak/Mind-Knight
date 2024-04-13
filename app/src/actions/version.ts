ff58fd18201af52d60f16b0a802d00080ccb9d7e
Nik <novakn@mcmaster.ca>
2024-04-13 03:55:21 -0400

    conhost attempt

--- app/src/actions/version.ts
+++ app/src/actions/version.ts
@@ -17,21 +17,22 @@ export async function getVersion(localPath:string, remotePath:string):Promise<Ve
 
 export async function updateVersion(){
   console.log('Starting update process...')
-  exec('cd .. && start call ./UPDATE-WINDOWS.bat', {encoding:'utf8'});
-  console.log('Started update process...')
+  // exec('cd .. && start call ./UPDATE-WINDOWS.bat', {encoding:'utf8'});
+  // console.log('Started update process...')
   await sleep(1000);
   console.log('Exiting...');
-  console.log('PPID:', process.pid);
-  console.log('PID:', process.pid);
-  console.log('trying: SIGKILL PPID');
-  process.kill(process.ppid, 'SIGKILL');
-  console.log('trying: SIGKILL PID');
-  process.kill(process.pid, 'SIGKILL');
-  console.log('trying: SIGQUIT');
-  process.kill(process.pid, 'SIGQUIT');
-  console.log('trying: SIGINT');
-  process.kill(process.pid, 'SIGINT');
-  console.log('trying: SIGHUP');
-  process.kill(process.pid, 'SIGHUP');
-  process.exit(1);
+  exec('Get-Process | Where-Object { $_.ProcessName -eq "conhost" } | Stop-Process -Force', {shell: 'powershell'});
+  // console.log('PPID:', process.pid);
+  // console.log('PID:', process.pid);
+  // console.log('trying: SIGKILL PPID');
+  // process.kill(process.ppid, 'SIGKILL');
+  // console.log('trying: SIGKILL PID');
+  // process.kill(process.pid, 'SIGKILL');
+  // console.log('trying: SIGQUIT');
+  // process.kill(process.pid, 'SIGQUIT');
+  // console.log('trying: SIGINT');
+  // process.kill(process.pid, 'SIGINT');
+  // console.log('trying: SIGHUP');
+  // process.kill(process.pid, 'SIGHUP');
+  // process.exit(1);
 }
