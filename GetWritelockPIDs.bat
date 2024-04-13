0503fdcc6e1c6a9eca368942b07846245f473e2f
Nik <novakn@mcmaster.ca>
2024-04-13 05:23:06 -0400

    reattempt

--- GetWritelockPIDs.bat
+++ GetWritelockPIDs.bat
@@ -1,3 +1,3 @@
 @echo off
 
-powershell.exe -Command ".\handle.exe $PWD | Select-String 'pid:' | ForEach-Object { $_ -replace '.*pid:\s*(\d+).*', '$1' }"
+powershell.exe -Command ".\handle.exe $PWD | Select-String 'pid:' | Where-Object { $_ -notmatch 'explorer.exe' } | ForEach-Object { $_ -replace '.*pid:\s*(\d+).*', '$1' }"
