57ba8707b45380873ea79d2cbc21c86438e3ae4f
Nik <novakn@mcmaster.ca>
2024-04-13 10:34:51 -0400

    better versioning text

--- app/src/components/Version/Version.tsx
+++ app/src/components/Version/Version.tsx
@@ -15,7 +15,7 @@ const Version:FC<Props> = async ({localPath, remotePath}) =>{
   // console.log(typeof version.local, typeof version.remote);
   return upToDate ? 
     <span>v{version.local} - (<Link style={{color: !upToDate ? 'red' : undefined}} href="/update">REINSTALL</Link>)</span>
-    : <span>v{version.local} - (<Link href="/update" style={{color:"red"}}>UPDATE</Link>)</span>
+    : <span>v{version.local} - (<Link href="/update" style={{color:"red"}}>UPDATE to {version.remote}</Link>)</span>
 };
 
 export default suspense(Version, 'checking version...');
