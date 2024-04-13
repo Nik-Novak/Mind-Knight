2c3efaf8c71a4b8d08a9fef1c91b20141d5deaef
Nik <novakn@mcmaster.ca>
2024-04-13 09:59:14 -0400

    reattempt

--- RUN-WINDOWS.bat
+++ RUN-WINDOWS.bat
@@ -7,7 +7,9 @@ REM if not exist ".\node_modules" (
     call npx yarn prisma generate
 REM )
 REM if not exist ".\.next\BUILD_ID" (
+    rmdir /s /q .\.next
     call npx yarn build:windows
+    rmdir /s /q .\.next
     call npx yarn build:windows
 REM )
 call npx yarn start:windows
