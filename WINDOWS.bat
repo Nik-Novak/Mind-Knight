cf8bcbd5688b6ab9969452a90be40c5240264180
Nik-Novak <novakn@mcmaster.ca>
2024-04-12 21:26:52 -0400

    Update WINDOWS.bat

--- WINDOWS.bat
+++ WINDOWS.bat
@@ -1,9 +1,11 @@
+set "PATH=%CD%;%PATH%"
 cd .\app
+call npx -v
+call node -v
 if not exist ".\node_modules" (
-    call ..\npx.cmd --yes yarn install
+    call npx yarn install
 )
-call ..\node.exe -v
-start call ..\npx.cmd --yes yarn windows
+start call npx yarn windows
 timeout /t 5
 start "" cmd /c "start http://localhost:3000 && taskkill /f /im cmd.exe"
 pause
