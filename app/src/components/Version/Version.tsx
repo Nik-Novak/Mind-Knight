1dd16810dfb7954529ec8624f59791ace79eac79
Nik <novakn@mcmaster.ca>
2024-04-14 06:21:33 -0400

    Remove console.logs

--- app/src/components/Version/Version.tsx
+++ app/src/components/Version/Version.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Version:FC<Props> = async ({localPath, remotePath}) =>{
   let version = await getVersion(localPath, remotePath);
   let upToDate = version.local == version.remote;
-  console.log('TEST', version.local, '-', version.remote);
+  // console.log('VERSION', version.local, '-', version.remote);
   // console.log(typeof version.local, typeof version.remote);
   return upToDate ? 
     <span>v{version.local} - (<Link style={{color: !upToDate ? 'red' : undefined}} href="/update">REINSTALL</Link>)</span>
