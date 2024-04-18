dbd922254119f6e5f72dd6847906aa1687f3618b
Nik <novakn@mcmaster.ca>
2024-04-17 21:14:06 -0400

    v1.1.0 release

--- app/src/components/Background/Background.tsx
+++ app/src/components/Background/Background.tsx
@@ -6,7 +6,6 @@ import { ReactNode } from "react"
 type Props = React.HTMLAttributes<HTMLDivElement>;
 export default function Background(props: Props){
   const map = useStore(state=>state.game?.game_found.Map) as GameMap|undefined;
-  console.log('MAP', map);
   return (
     <main id='content' style={{background: map!=undefined && GameMap[map] ? `url(/img/maps/${GameMap[map]}.png) center/cover no-repeat` : '#222'}} {...props}>
       {props.children}
