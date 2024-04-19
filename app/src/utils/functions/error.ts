02af3e3e39ba9ee8852f77023db4546d198ec0c9
Nik <novakn@mcmaster.ca>
2024-04-19 09:41:48 -0400

    remove db spam

--- app/src/utils/functions/error.ts
+++ app/src/utils/functions/error.ts
@@ -7,15 +7,15 @@ export async function attempt<R>(thunk:AsyncThunk<R>, game_id:string, context?:s
         return await thunk();
       }catch(err:unknown){
         if(err instanceof Error)
-          await database.rawGame.create({
-            data:{
-              data: LogTailer.readLog(),
-              upload_reason:'Error',
-              error: err.message + '\n' + err.stack,
-              context,
-              game_id
-            }
-          })
+          // await database.rawGame.create({
+          //   data:{
+          //     data: LogTailer.readLog(),
+          //     upload_reason:'Error',
+          //     error: err.message + '\n' + err.stack,
+          //     context,
+          //     game_id
+          //   }
+          // })
         console.error(err);
       }
 }
