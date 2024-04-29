57fc2242c8c06f1bb070de87812cb9a89253f6eb
Nik <novakn@mcmaster.ca>
2024-04-29 01:58:55 -0400

    added -y to install

--- RUN-WINDOWS.bat
+++ RUN-WINDOWS.bat
@@ -4,7 +4,7 @@ cd .\app
 call npx -v
 call node -v
 @REM if not exist ".\node_modules" (
-    call npx yarn install --network-timeout 100000
+    call npx yarn install -y --network-timeout 100000
     call npx yarn prisma generate
 @REM )
 if not exist ".\.next\BUILD_ID" (
