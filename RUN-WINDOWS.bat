292c7e57c36068b5a594be6ce18ee6ec25f0ed39
Nik <novakn@mcmaster.ca>
2024-05-02 04:25:57 -0400

    2.2.0 - bug reports -  bugfix

--- RUN-WINDOWS.bat
+++ RUN-WINDOWS.bat
@@ -1,20 +1,16 @@
 @echo off
 set "PATH=%CD%;%PATH%"
 cd .\app
-
-call npx -v 2>&1 | tee.bat .\log\console.log
-call node -v 2>&1 | tee.bat .\log\console.log
-
+call npx -v
+call node -v
 @REM if not exist ".\node_modules" (
-    call npx yarn install -y --network-timeout 100000 2>&1 | tee.bat .\log\console.log
-    call npx yarn prisma generate 2>&1 | tee.bat .\log\console.log
+    call npx yarn install -y --network-timeout 100000
+    call npx yarn prisma generate
 @REM )
-
 if not exist ".\.next\BUILD_ID" (
     @REM rmdir /s /q .\.next
     @REM call npx yarn build:windows
     @REM rmdir /s /q .\.next
-    call npx yarn build:windows 2>&1 | tee.bat .\log\console.log
+    call npx yarn build:windows
 )
-
-call npx yarn start:windows 2>&1 | tee.bat .\log\console.log
+call npx yarn start:windows
